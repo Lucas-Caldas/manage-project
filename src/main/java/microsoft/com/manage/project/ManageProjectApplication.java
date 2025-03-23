e3b94e2864fe97e484cc2647d3bba8fc327377ba
Lucas-Caldas <caldas.oliva@gmail.com>
2025-03-23 09:32:54 -0300

    start of application

---
+++ src/main/java/microsoft/com/manage/project/ManageProjectApplication.java
@@ -0,0 +1,13 @@
+package microsoft.com.manage.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microsoft/com/manage/project/ManageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package microsoft.com.manage.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
